e2a406201021ae7fef9c9f437ce5ad387516bc11
platinumazure <platinumazure@gmail.com>
2024-02-10 20:58:43 -0600

    feat: Send test message to Mix It Up webhook

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+import { Client } from "node-rest-client";
+import config from "./config.json" assert { type: "json" };
+
 console.log("Welcome to the control program for Midnight Melody!");
 console.log("Check back later on as features are added.");
+
+const mixItUpUrl = `https://api.mixitupapp.com/api/webhook/${config.mixItUp.webhookId}?secret=${config.mixItUp.secret}`;
+
+const client = new Client();
+
+client.post(
+    mixItUpUrl,
+    {
+        data: {
+            message: "Here is a message"
+        },
+        headers: {
+            "Content-Type": "application/json"
+        }
+    },
+    (data, response) => {
+        console.log(`Status code: ${response.statusCode}`);
+        console.log(`Status code: ${response.statusMessage}`);
+        console.log(`Response body: ${data.toString("utf8")}`);
+    }
+);
