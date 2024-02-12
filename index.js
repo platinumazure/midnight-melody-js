854bc4c62bcb324015778546bff98ef2f1a813ff
platinumazure <platinumazure@gmail.com>
2024-02-11 23:21:22 -0600

    feat(llm): Create LLM class wrapper
    Create a class wrapper for the LLM code. This will begin to track context/state across more than one round of prompting.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import { getLlmResponse } from "./src/llm/gpt4allModel.js";
+import { Gpt4allModel } from "./src/llm/gpt4allModel.js";
 import { sendTts } from "./src/textToSpeech/mixItUp.js";
 
 console.log("Welcome to the control program for Midnight Melody!");
 console.log("Check back later on as features are added.");
 
-const ttsResponse = await getLlmResponse();
+const model = new Gpt4allModel();
 
-await sendTts(ttsResponse);
+const userPrompts = [
+    "What is the meaning of life?",
+];
+
+for (const message of userPrompts) {
+    console.log(`Sending user message: "${message}"`);
+
+    const responseContent = await model.getResponse(message);
+    console.log(`Received model response: "${responseContent}"`);
+
+    await sendTts(responseContent);
+}
--- src/llm/gpt4allModel.js
+++ src/llm/gpt4allModel.js
@@ -4,21 +4,49 @@ import { createCompletion, loadModel } from "gpt4all";
 const systemPromptUrl = new URL("../../config/MidnightMelodySystemPrompt.txt", import.meta.url);
 const systemPrompt = await fs.readFile(systemPromptUrl);
 
-export async function getLlmResponse() {
-    const model = await loadModel("gpt4all-falcon-newbpe-q4_0.gguf");
+const model = await loadModel("gpt4all-falcon-newbpe-q4_0.gguf");
 
-    const response = await createCompletion(
-        model,
-        [
-            { role: 'system', content: systemPrompt },
-            { role: 'user', content: "What is the meaning of life?" },
-        ],
-        {
-            systemPromptTemplate: "%1",
-            promptTemplate: "### Instruction:\n%1\n### Response:\n"
-        }
-    );
+export class Gpt4allModel {
+    constructor() {
+        this._messages = getInitialMessages();
+    }
 
-    const ttsResponse = response.choices[0].message.content;
-    return ttsResponse;
+    async getResponse(userMessage) {
+        this._messages.push({
+            role: "user",
+            content: userMessage,
+        });
+
+        const response = await createCompletion(
+            model,
+            this._messages,
+            {
+                systemPromptTemplate: "%1",
+                promptTemplate: "### Instruction:\n%1\n### Response:\n"
+            }
+        );
+
+        this._messages.push(response);
+    
+        const responseContent = response.choices[0].message.content;
+        return responseContent;
+    }
+
+    get lastResponse() {
+        return this._messages
+            .findLast(m => m.role === "assistant")
+            ?.content;
+    }
+
+    get lastPrompt() {
+        return this._messages
+            .findLast(m => m.role === "user")
+            ?.content;
+    }
+}
+
+function getInitialMessages() {
+    return [
+        { role: 'system', content: systemPrompt },
+    ];
 }
